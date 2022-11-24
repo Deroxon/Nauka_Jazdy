92031060005546467b75d3b99f1f22819a7bf88e
FUEWROCLAW\LskD <lskd@viessmann.com>
2022-11-24 08:14:26 +0100

    Changed SEO text and adjustment of description for mobile view

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -9,26 +9,28 @@ export class DescriptionComponent implements OnInit {
 
   width: number = window.innerWidth
 
+
   texts = [
-    { h2: 'Szkoła jazdy Trzebnica (Prusice) – co oferujemy?',
-      p:  'OSK Julita Lasek zaprasza do nowoczesnej szkoły jazdy w Prusicach. Oferujemy kompleksowy kurs prawa jazdy, który przygotuje każdego do rozpoczęcia przygody za kierownicą i zdania państwowego egzaminu. Kurs składa się z 30 godzin części teoretycznej oraz 30 godzin części praktycznej (jazd). Nasi kursanci otrzymają także dostęp do materiałów szkoleniowych oraz 4 godziny profesjonalnych zajęć z ratownikiem medycznym, który w kompleksowy sposób przeprowadzi zajęcia z pierwszej pomocy.'
-    },
-    { h2: 'W jaki sposób odbywa się część teoretyczna kursu?',
+    { h3: 'W jaki sposób odbywa się część teoretyczna kursu',
       p:'Nasz kurs przewiduje teoretyczną naukę jazdy wyłącznie w wersji e-learningowej. Kursanci OSK dostaną dostęp do wykładów oraz materiałów, które będą mogli w dowolnej chwili odtworzyć w domu. Platforma, na której będą się poruszać, zawiera także testy i egzaminy, dzięki którym będą mogli sprawdzić swoją wiedzę na temat opanowanego materiału. Zajęcia z ratownikiem medycznym, które wchodzą w skład części teoretycznej, będą odbywać się na miejscu w OSK – szkole jazdy w Prusicach.'
     },
-    { h2: 'Czy OSK Julita Lasek oferuje naukę jazdy w Trzebnicy?',
-      p:'Szkoła jazdy OSK Julita Lasek ma swój budynek w Prusicach, ale przewidujemy naukę jazdy z dojazdem do kursanta w zakresie 10 kilometrów – oferujemy dojazd do uczestników kursu prawa jazdy z Trzebnicy, Żmigrodu, Prusic i okolicy Prusic. Jazdy natomiast odbywać się będą w większym zakresie kilometrów – w tym w Miliczu, gdzie kursanci będą ćwiczyć na placu manewrowym.'
-    },
-    { h2: 'Co potrzeba, aby rozpocząć kurs na prawo jazdy? ',
-      p:'Aby rozpocząć kurs na prawo jazdy, należy wyrobić numer PKK. Z doświadczenia wiemy, że składanie tego rodzaju dokumentów stanowi kłopot dla początkujących kierowców, dlatego przeprowadzimy Cię przez cały proces. Jak więc to zrobić? Wystarczy stawić w biurze OSK Prusice z wnioskiem do Wydziału Komunikacji, który pomożemy Ci wypełnić na miejscu. Do wyrobu numeru potrzebujesz także zdjęcia dowodowego oraz badania lekarskiego.'
-    },
-    { h2: 'Jak mogę zapisać się na kurs w szkole jazdy? (Trzebnica, Prusice, Żmigród)',
-      p:'Jest kilka możliwości zapisania się do naszej szkoły jazdy w Prusicach. Na kurs prawa jazdy zapiszesz się poprzez kontakt telefoniczny, mailowy lub przez wiadomość prywatną na naszym profilu facebookowym. Oczywiście, zapraszamy także do biura OSK, które znajduje się w Prusicach przy ulicy Ludowej 4 – zapiszesz się tam na miejscu!'
-    },
-    {
-      h2: 'Czy kurs prawo jazdy Prusice w szkole jazdy OSK przygotowuje do zdania egzaminu w Miliczu?',
-      p: 'Nasza szkoła jazdy w Trzebnicy (Prusice) oferuje kompleksowe przygotowanie do państwowego egzaminu na prawo jazdy. Milicz jest najczęstszym wyborem naszych kursantów, jeśli chodzi o przystąpienie do egzaminu teoretycznego oraz praktycznego. Tam też przeprowadzamy lekcje na placu manewrowym DORD.'
-    },
+    { h3: 'Czy OSK Julita Lasek oferuje naukę jazdy w Trzebnicy?',
+    p:'Szkoła jazdy OSK Julita Lasek ma swój budynek w Prusicach, ale przewidujemy naukę jazdy z dojazdem do kursanta w zakresie 10 kilometrów – oferujemy dojazd do uczestników kursu prawa jazdy z Trzebnicy, Żmigrodu, Prusic i okolicy Prusic. Jazdy natomiast odbywać się będą w większym zakresie kilometrów – w tym w Miliczu, gdzie kursanci będą ćwiczyć na placu manewrowym.'
+  },
+  { h3: 'Co potrzeba, aby rozpocząć kurs na prawo jazdy? ',
+    p:'Aby rozpocząć kurs na prawo jazdy, należy wyrobić numer PKK. Z doświadczenia wiemy, że składanie tego rodzaju dokumentów stanowi kłopot dla początkujących kierowców, dlatego przeprowadzimy Cię przez cały proces. Jak więc to zrobić? Wystarczy stawić w biurze OSK Prusice z wnioskiem do Wydziału Komunikacji, który pomożemy Ci wypełnić na miejscu. Do wyrobu numeru potrzebujesz także zdjęcia dowodowego oraz badania lekarskiego.'
+  },
+  { h3: 'Jak mogę zapisać się na kurs w szkole jazdy?',
+    p:'Jest kilka możliwości zapisania się do naszej szkoły jazdy w Prusicach. Na kurs prawa jazdy zapiszesz się poprzez kontakt telefoniczny, mailowy lub przez wiadomość prywatną na naszym profilu facebookowym. Oczywiście, zapraszamy także do biura OSK, które znajduje się w Prusicach przy ulicy Ludowej 4 – zapiszesz się tam na miejscu!'
+  },
+  {
+    h3: 'Czy kurs prawo jazdy Prusice w szkole jazdy OSK przygotowuje do zdania egzaminu w Miliczu?',
+    p: 'Nasza szkoła jazdy w Prusicach (Trzebnicy) oferuje kompleksowe przygotowanie do państwowego egzaminu na prawo jazdy. Milicz jest najczęstszym wyborem naszych kursantów, jeśli chodzi o przystąpienie do egzaminu teoretycznego oraz praktycznego. Tam też przeprowadzamy lekcje na placu manewrowym DORD – uczestnicy kursu mogą więc liczyć na kompleksowe przygotowanie do zdawania egzaminu w tym właśnie mieście.'
+  },
+  {
+    h3: 'Czy mogę zapłacić za kurs w ratach?',
+    p: 'Tak! Płatność za pełny kurs prawa jazdy, może zostać rozłożona na raty które uzgadniamy indywidualnie, możliwość wpłacenia pierwszej raty jest już od 300zł!'
+  }
   ]
 
   constructor() { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBGz5zUVP2CKYpJnVwmGJfNorrtheH87KI",
+    authDomain: "student-manager-system-f7773.firebaseapp.com",
+    projectId: "student-manager-system-f7773",
+    storageBucket: "student-manager-system-f7773.appspot.com",
+    messagingSenderId: "306569945904",
+    appId: "1:306569945904:web:109504bb7a18677a255ef5"
+  }
+  
+  
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
