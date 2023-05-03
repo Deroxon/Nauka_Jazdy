f002209ef73f67b3903edbc01688da83e8311b61
Deroxon <deroxon@gmail.com>
2023-05-03 14:47:15 +0200

    changed pricing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBGz5zUVP2CKYpJnVwmGJfNorrtheH87KI",
+    authDomain: "student-manager-system-f7773.firebaseapp.com",
+    projectId: "student-manager-system-f7773",
+    storageBucket: "student-manager-system-f7773.appspot.com",
+    messagingSenderId: "306569945904",
+    appId: "1:306569945904:web:109504bb7a18677a255ef5"
+  }
 };
 
 /*
